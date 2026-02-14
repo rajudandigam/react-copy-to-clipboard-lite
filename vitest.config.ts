a282b6360e57a2fe5581b1fe7d53375d7840b72a
rajudandigam <dandigam.raju@gmail.com>
2026-02-14 00:41:55 -0800

    udpated scripts for publish

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    exclude: ["**/node_modules/**", "**/dist/**", "playwright/**"],
     environment: "jsdom",
     globals: true,
     clearMocks: true,
